7f320bef073166cbd75a1f874694e48cbf6f01bd
AGolumbovsky <agolumbovsky@gmail.com>
2015-07-11 00:22:50 -0500

    obj.fn

---
+++ codeExamples.js
@@ -0,0 +1,19 @@
+// weird and awesome ways to access the object's elements
+var arr = [
+    {
+        name: "obama", 
+        number: 44,
+        first_term: true
+    },
+    
+    function(number) {
+        //change the number if re-elected
+        arr[0].number ++;
+        console.log(arr[0].number);
+        console.log("something");
+        
+    }
+    
+];
+
+arr[1](arr[0].number);
